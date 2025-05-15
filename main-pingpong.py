0789142a618029134fee5c323612a55673c33df4
JolteonGames628 <apenguingod61@gmail.com>
2025-05-15 19:58:18 +0200

    Update main-pingpong.py
    I addet player and more but it can´t still move.

--- main-pingpong.py
+++ main-pingpong.py
@@ -1,6 +1,6 @@
 from pygame import *
 
-SCREEN_WIDTH = 800
+SCREEN_WIGHT = 800
 SCREEN_HEIGHT = 600
 FPS = 60
 screen = display.set_mode((800,600))
@@ -8,12 +8,58 @@ display.set_caption("Ping-Pong")
 
 clock = time.Clock()
 
+class GamesSprite(sprite.Sprite):
+    def __init__(self, x, y, speed, wight, height, color):
+        self.speed = speed
+        self.color = color
+        self.rect = Rect(x, y, wight, height)
+    def draw(self):
+        draw.rect(screen, self.color, self.rect)
+
+player1 = GamesSprite(30, 200, 5, 30, 150, "red")
+player2 = GamesSprite(740, 200, 5, 30, 150, "blue")
+
 running = True
 while running:
     for ev in event.get():
         if ev.type == QUIT:
             running = False
     
+    player1.draw()
+    player2.draw()
+    display.update()
+    clock.tick(FPS)
+
+quit()from pygame import *
+
+SCREEN_WIGHT = 800
+SCREEN_HEIGHT = 600
+FPS = 60
+screen = display.set_mode((800,600))
+display.set_caption("Ping-Pong")
+
+clock = time.Clock()
+
+class GamesSprite(sprite.Sprite):
+    def __init__(self, x, y, speed, wight, height, color):
+        self.speed = speed
+        self.color = color
+        self.rect = Rect(x, y, wight, height)
+    def draw(self):
+        draw.rect(screen, self.color, self.rect)
+
+player1 = GamesSprite(30, 200, 5, 30, 150, "red")
+player2 = GamesSprite(740, 200, 5, 30, 150, "blue")
+
+running = True
+while running:
+    for ev in event.get():
+        if ev.type == QUIT:
+            running = False
+    
+    player1.draw()
+    player2.draw()
+    display.update()
     clock.tick(FPS)
 
 quit()
